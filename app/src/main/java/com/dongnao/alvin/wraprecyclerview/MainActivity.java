5a39431c45812c8abd51a5d1cbef7958c15b0a6e
Lython <liu.junli@moxiangroup.com>
2018-09-21 15:21:53 +0800

    init

---
+++ app/src/main/java/com/dongnao/alvin/wraprecyclerview/MainActivity.java
@@ -0,0 +1,47 @@
+package com.dongnao.alvin.wraprecyclerview;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends com.lythonliu.LinkAppCompatActivity {
+    @Override
+    public String getAppName(){
+        return BuildConfig.APP_NAME;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WrapRecyclerView recyclerView = (WrapRecyclerView) findViewById(R.id.recycler_view);
+
+        TextView headerView = new TextView(this);
+        ViewGroup.LayoutParams layoutParams = new ActionBar.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        headerView.setLayoutParams(layoutParams);
+        headerView.setText("HeaderView");
+
+        recyclerView.addHeaderView(headerView);
+        TextView footerView = new TextView(this);
+        layoutParams = new ActionBar.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        footerView.setLayoutParams(layoutParams);
+        footerView.setText("FooterView");
+        recyclerView.addFooterView(footerView);
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            list.add(i, "Item " + i);
+        }
+        MyAdapter adapter = new MyAdapter(list);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
